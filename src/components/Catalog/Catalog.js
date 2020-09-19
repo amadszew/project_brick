645efb3ad5b625dbdd2a0b7734c0f7f129ec74a9
Amadeusz <amadeusz0szewczyk@gmail.com>
2020-09-19 15:49:49 +0200

    build structure for themes and sets

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
+import 'core-js/features/array/is-array';
+
 import { Themes } from './Themes/Themes';
 import { Sets } from './Sets/Sets';
 
@@ -11,7 +13,7 @@ export const Catalog = () => {
   const [sets, setSets] = useState([]);
 
   const APIThemes = 'https://rebrickable.com/api/v3/lego/themes/?key=348ddf6de615ae3d89f79a2f46007745&page_size=1000';
-  const APISets = `https://rebrickable.com/api/v3/lego/sets/?key=348ddf6de615ae3d89f79a2f46007745&page_size=100&theme_id=${selectedSubtheme}`;
+  const APISets = 'https://rebrickable.com/api/v3/lego/sets/?key=348ddf6de615ae3d89f79a2f46007745';
 
   useEffect(() => {
     //FETCH THEMES LIST
@@ -29,22 +31,9 @@ export const Catalog = () => {
       .catch(err => {
         console.log(err.message);
       })
-  }, []);
 
-  useEffect(() => {
-    
-    setSubthemes(
-      themes.filter(theme => 
-        theme.id === selectedTheme || 
-        theme.id === selectedSubtheme || 
-        theme.parent_id === selectedTheme || 
-        theme.parent_id === selectedSubtheme
-      )
-    )
-    
     //FETCH SETS LIST
-    {selectedSubtheme && (
-      fetch(APISets)
+    fetch(APISets)
       .then(response => {
         if (response.ok) {
           return response.json();
@@ -58,19 +47,73 @@ export const Catalog = () => {
       .catch(err => {
         console.log(err.message);
       })
-    )}
-  }, [selectedTheme, selectedSubtheme]);
+  }, []);
+
+    useEffect(() => {     
+      const setsMap = {}
+      sets.forEach(set => {
+        if (setsMap[set.theme_id]) {
+          if(Array.isArray(setsMap[set.theme_id])){
+            setsMap[set.theme_id].push(set);
+          } else {
+            setsMap[set.theme_id] = [];
+            setsMap[set.theme_id].push(set);
+          }
+        } else {
+          setsMap[set.theme_id] = [];
+          setsMap[set.theme_id].push(set);
+        } 
+      });
+      console.log('setsMap', setsMap);
+
+      // SETING THEME
+      let parents = {};
+      let parent = null;
+      let child = null;
+      let grandChild = null;
+      themes.forEach(theme => {
+        //set parent theme
+        if (theme.parent_id === null) {
+          parents[theme.id] = {
+            ...theme,
+            children: {}
+          }
+          parent = parents[theme.id];
+        }
+
+        //set child
+        if (theme.parent_id === parent.id) {
+          parent.children[theme.id] = {
+            ...theme,
+            children: {},
+          }
+        
+          child = parent.children[theme.id];
+        }
+
+        //set granchild
+        if(child && theme.parent_id === child.id) {
+          child.children[theme.id] = {
+            ...theme,
+            children: {},
+          }
+
+          grandChild = child.children[theme.id]
+        }
+      });
+      console.log(parents, 'object parent')
+    }, [themes, sets]);
 
   return (
     <main className="catalog">
-      <Themes 
+      {/* <Themes 
         themes={themes} 
         selectedTheme={selectedTheme} 
         subthemes={subthemes} 
         setSelectedTheme={setSelectedTheme} 
         setSelectedSubtheme={setSelectedSubtheme}
-      />
-      <Sets sets={sets} />
+      /> */}
+      {/* <Sets sets={sets} /> */}
     </main>
   );
 }
