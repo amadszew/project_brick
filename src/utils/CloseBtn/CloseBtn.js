622296f4dcdf5afc7b48da00cee8181b196dcd3a
Amadeusz <amadeusz0szewczyk@gmail.com>
2020-10-04 10:46:55 +0200

    return button - styles fixes

--- src/utils/CloseBtn/CloseBtn.js
+++ src/utils/CloseBtn/CloseBtn.js
@@ -4,7 +4,7 @@ export const CloseBtn = ({ callback }) => {
   return (
     <button className="close-button" onClick={callback}>
       <div className="close-button__sign">
-        <span className="close-button__label">Back</span>
+        <span className="close-button__label">Close</span>
       </div>
     </button>
   );
